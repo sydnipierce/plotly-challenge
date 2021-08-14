3bd7a8bd78292eb33a4724a58465beca2f6202f2
Sydni <sydnimpierce@gmail.com>
2021-08-14 16:45:48 -0400

    started html object refs

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,6 @@
+// Select relevant HTML elements
+var bar = d3.select("#bar")
+
 // Read belly button data
 d3.json("../data/samples.json").then((data) => {
     // console.log(data)
@@ -13,17 +16,7 @@ d3.json("../data/samples.json").then((data) => {
     var values = samples.map(x => x.sample_values);
     console.log(otu_ids);
     console.log(values);
-
-    function toObject(keys, values) {
-        var ind_otus = [];
-        var result = {};
-        for (var i = 0; i < keys.length; i++)
-             result[keys[i]] = values[i]
-             ind_otus.push(result);
-        return ind_otus;
-    }
-
-    console.log(toObject(otu_ids, values))
     
 });
 
+var selection = d3.select("#selDataset").property("value")
