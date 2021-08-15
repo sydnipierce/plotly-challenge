5499c6371a289d0def266f87209bb88bdc5d2c9f
Sydni <sydnimpierce@gmail.com>
2021-08-15 12:41:09 -0400

    built out chart refresh structure

--- static/js/app.js
+++ static/js/app.js
@@ -26,9 +26,13 @@ d3.json("../data/samples.json").then((data) => {
         data = [{
           x: [1, 2, 3, 4, 5],
           y: [1, 2, 4, 8, 16] }];
+
+        // var bardata = [trace1];
+        // var gaugedata = [trace2];
+        // var bubbledata = [trace3];
       
         Plotly.newPlot("plot", data);
-      }
+      };
     
     // Populate dropdown with IDs
     d3.select("#selDataset").selectAll("option").data(names)
@@ -38,22 +42,39 @@ d3.json("../data/samples.json").then((data) => {
         return d;
     });
 
-    // 
-    // var bardata = [trace1];
-    // var gaugedata = [trace2];
-    // var bubbledata = [trace3];
-
     // Create event listener
     d3.select("#selDataset").on("change", refresh());
 
     // Define function (chart update) for event listener
     function refresh() {
+        // Collect ID selected by user
         var input = d3.select("#selDataset").property("value");
 
-    }
+        // Create x and y arrays for bar chart
+        var x = [];
+        var y = [];
+
+        for 
+            if (input === 'names[i]') {
+                x = [1, 2, 3, 4, 5];
+                y = [1, 2, 4, 8, 16];
+            }
+
+            else if (dataset === 'dataset2') {
+                x = [10, 20, 30, 40, 50];
+                y = [1, 10, 100, 1000, 10000];
+            }
+        
+        // Note the extra brackets around 'x' and 'y'
+        Plotly.restyle("plot", "x", [x]);
+        Plotly.restyle("plot", "y", [y]);
+        }
+    };
 
     // function refresh() {
     //     plotly.(bar, trace1);
     //     plotly.
     // };
+
+    init();
 });
