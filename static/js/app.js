12ae8577fbca99612e92355ad7659d44fde1a203
Sydni <sydnimpierce@gmail.com>
2021-08-15 16:54:26 -0500

    creating bubble chart

--- static/js/app.js
+++ static/js/app.js
@@ -12,45 +12,62 @@ d3.json("../data/samples.json").then((data) => {
 
     var otu_ids = samples.map(x => x.otu_ids);
     var otu_labels = samples.map(x => x.otu_labels);
-    console.log(otu_ids);
+    // console.log(otu_ids);
 
     // Initialize charts
     function init() {
         // Create initial chart variables
-        // var id = names[0];
         var meta = metadata[0];
-        // var gender = metadata[0].gender;
-        // var age = metadata[0].age;
-        // var location = metadata[0].location;
-        // var bbtype = metadata[0].bbtype;
-        // var wfreq = metadata[0].wfreq;
-        var barx = samples[0].sample_values.slice(0, 10);
-        var bary = otu_ids[0].slice(0, 10);
-        var barhov = otu_labels[0].slice(0, 10);
+        var val = samples[0].sample_values;
+        var iden = otu_ids[0];
+        var lab = otu_labels[0];
 
         // Populate dropdown with IDs
         Object.entries(meta).forEach(([key, value]) => {
             // Log the key and value
-            d3.select("#sample-metadata")
+            d3.select("#sample-metadata").select("p")
+            .data([key, value])
+            .enter()
             .append("p")
-            .text('${key} value');
-            console.log(`Key: ${key} and Value ${value}`)
+            .text(function(d) {return d.key, d.value;});
+            // console.log(`Key: ${key} and Value ${value}`)
         });
 
         // Create initial chart traces
         var bartrace = {
             type: 'bar',
-            x: barx,
-            y: bary,
+            x: val.slice(0, 10),
+            y: iden.slice(0, 10),
+            text: lab.slice(0, 10),
             orientation: 'h'
+        };
+
+        var bubtrace = {
+            x: iden,
+            y: val,
+            mode: 'markers',
+            marker: {
+                size: val
+            }
+        };
+
+        // Create initial layouts
+        var barlayout = {
+            yaxis: {
+                type: 'category',
+                title: 'OTU ID'
+            }
         };
 
         // Create initial chart data
         var bardata = [bartrace];
+        var bubdata = [bubtrace];
       
-        Plotly.newPlot("bar", bardata);
+        Plotly.newPlot("bar", bardata, barlayout);
+        // Plotly.newPlot("bubble", bubdata, bublayout);
+        // Plotly.newPlot("gauge", gaugedata, gaugelayout);
 
-        d3.select("#sample-metadata").html('<p>ID: ${id}</p><p>ETHNICITY: ${ethnicity}</p><p>GENDER: ${gender}</p><p>AGE: ${age}</p><p>LOCATION: ${location}</p><p>BBTYPE: ${bbtype}</p><p>WFREQ: ${wfreq}</p>');
+        // d3.select("#sample-metadata").html('<p>ID: ${id}</p><p>ETHNICITY: ${ethnicity}</p><p>GENDER: ${gender}</p><p>AGE: ${age}</p><p>LOCATION: ${location}</p><p>BBTYPE: ${bbtype}</p><p>WFREQ: ${wfreq}</p>');
     };
     
     // Populate dropdown with IDs
